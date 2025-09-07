6d542fa38d3c64a392e784c5e023d692fd0086eb
Dariusz Okoński <darek200180@gmail.com>
2025-09-07 12:20:16 +0200

    add socket.io to server

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const path = require('path');
+const socket = require('socket.io');
 
 const app = express();
+
+// Serve static files from the 'client' directory
+app.use(express.static(path.join(__dirname, 'client')));
 
 const PORT = process.env.PORT || 8000;
 
@@ -10,4 +15,31 @@ const server = app.listen(process.env.PORT || 8000, () => {
 
 app.use((req, res) => {
   res.status(404).send({ message: 'Not found...' });
+});
+
+const io = socket(server);
+
+io.on('connection', (socket) => {
+  // socket.on('join', (incomingClient) => {
+  //   const client = { id: socket.id, name: incomingClient.author };
+  //   users.push(client);
+  //   const message = {
+  //     author: 'Chat Bot',
+  //     content: `${client.name} has joined the conversation!`,
+  //   };
+  //   socket.broadcast.emit('message', message);
+  // });
+  // socket.on('message', (message) => {
+  //   messages.push(message);
+  //   socket.broadcast.emit('message', message);
+  // });
+  // socket.on('disconnect', () => {
+  //   const clientName = users.find((user) => user.id === socket.id).name;
+  //   const message = {
+  //     author: 'Chat Bot',
+  //     content: `${clientName} has left the conversation...`,
+  //   };
+  //   socket.broadcast.emit('message', message);
+  //   users = users.filter((user) => user.id !== socket.id);
+  // });
 });
